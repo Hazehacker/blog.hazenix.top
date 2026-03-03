8b97bcbecf97877792a25efd38aabcb8c475d73d
HazeHacker <3542495583@qq.com>
2026-03-03 21:17:59 +0800

    refactor: Adjust directory structure and remove unused files

---
+++ frontend/src/components/common/LoadingSpinner.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-center items-center py-8">
+    <el-icon class="is-loading text-2xl text-primary">
+      <Loading />
+    </el-icon>
+  </div>
+</template>
+
+<script setup>
+import { Loading } from '@element-plus/icons-vue'
+</script>
